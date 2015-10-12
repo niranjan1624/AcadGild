c82cfdac748c2ff029661fc8d40af2a25b618f17
niranjan1624 <niranjan.ece1@gmail.com>
2015-10-12 09:30:31 +0530

    Session1 Assignments added

---
+++ src/com/niranjan/ai/session1/Assignments.java
@@ -0,0 +1,31 @@
+package com.niranjan.ai.session1;
+
+public class Assignments {
+	
+	public static void main(String[] args) {
+		// Assignment 1;
+		int i = 10;
+		float f = (float) 10.1;
+		int output = i + (int)f;
+		System.out.println("Addition: " + output);
+		
+		//Assignment 2
+		int a, b = 4, c;
+		
+		//Assignment 3
+	  /*  Examples of data that can't be stored in int --> String, boolean
+		int x = true;
+		int y ="string";*/
+		
+		// Assignment 4
+		
+		int x = 5;
+		int y = 10;
+		
+		System.out.println("a. x + y * 2 = " + ( x + y * 2));
+		System.out.println("b. x - y + 2 = " + ( x - y + 2));
+		System.out.println("c. (x + y) * 2 = " + (( x + y) * 2));
+		System.out.println("d. y % x = " + ( y % x));
+		
+	}
+}
